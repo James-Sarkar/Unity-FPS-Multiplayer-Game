8769913ccc13cffc3d0db37ec81eb2eba66a5086
James Sarkar <James.Sarkar@sap.com>
2018-01-14 18:41:06 -0500

    Added crosshair and bullet impact effect

---
+++ Assets/Scripts/CursorController/CursorController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorController : MonoBehaviour {
+
+	void Start () {
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		ControlCursor ();
+	}
+
+	// Lock or unlock the cursor
+	void ControlCursor() {
+		if (Input.GetKeyDown (KeyCode.Tab)) {
+			if (Cursor.lockState == CursorLockMode.Locked) {
+				Cursor.lockState = CursorLockMode.None;
+			} else {
+				Cursor.lockState = CursorLockMode.Locked;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/FPS Character Scripts/FPSShootingControls.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class FPSShootingControls : NetworkBehaviour {
+
+	private Camera mainCam;
+
+	private float fireRate = 15f, nextTimeToFire = 0f;
+
+	[SerializeField]
+	private GameObject concreteImpact;
+
+	// Use this for initialization
+	void Start () {
+		mainCam = Camera.main;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Shoot ();
+	}
+
+	void Shoot() {
+		if (Input.GetMouseButtonDown (0) && Time.time > nextTimeToFire) {
+			nextTimeToFire = (Time.time + 1f) / fireRate;
+
+			RaycastHit hit;
+
+			if (Physics.Raycast (mainCam.transform.position, mainCam.transform.forward, out hit)) {
+				Instantiate (concreteImpact, hit.point, Quaternion.LookRotation (hit.normal));
+			}
+		}
+	}
+}
